649dacf212980761a86844d1cbe746ae50a5cd02
Danila <zzowich@gmail.com>
2022-07-25 23:45:29 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMoviesDB = {
 	count: numberOfFilms,
