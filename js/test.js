800ae61f2f8a0b1d3b222fac9e7d4801c557e911
Danila <zzowich@gmail.com>
2022-07-25 19:10:16 +0300

    first commit

---
+++ js/test.js
@@ -0,0 +1,12 @@
+function disemvowel(str) {
+  const vowelsArr = ['a', 'e', 'u', 'i', 'o']
+  const vowelsArrUpper = ['A', 'E', 'U', 'I', 'O']
+  for (let i = 0; i < str.length; i++) {
+   
+  }
+  
+  return str;
+}
+
+let str = 'This website is for losers LOL!'
+console.log(disemvowel(str))
