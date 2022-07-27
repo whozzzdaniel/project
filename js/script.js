e76681516c4e6b36aea387e6a68afb6b09706d70
Danila <zzowich@gmail.com>
2022-07-27 13:25:50 +0300

    update

--- js/script.js
+++ js/script.js
@@ -15,24 +15,42 @@ const personalMoviesDB = {
 /*
 let counter = 0;
 
-while (true) {
-	if (counter === 2) {
-		break;
-	} else {
-		const recentFilm = prompt('Какой фильм вы посмотрели недавно?', '');
-		const recentFilmMark = +prompt('На сколько его оцените?', 0);
+while (counter != 2) {
+	const recentFilm = prompt('Какой фильм вы посмотрели недавно?', ''),
+				recentFilmMark = +prompt('На сколько его оцените?', 0);
 
-		if (recentFilm === '' || recentFilm === null || recentFilm.length > 50) {
-			continue;
-		} else if (isNaN(recentFilmMark)) {
-			continue;
-		} else {
-			personalMoviesDB.movies[recentFilm] = recentFilmMark;
-			counter++;
-		}
+	if (recentFilm != '' && recentFilm != null && recentFilm.length <= 50 && isNaN(recentFilmMark) === false) {
+		personalMoviesDB.movies[recentFilm] = recentFilmMark;
+		counter++;
+		console.log('done');
+	} else {
+		console.log('error');
+		continue;
 	}
 }
 */
+
+// Вариант решения через цикл do while
+
+/*
+let counter = 0;
+
+do {
+		const recentFilm = prompt('Какой фильм вы посмотрели недавно?', ''),
+					recentFilmMark = +prompt('На сколько вы его оцените?', 0);
+		
+		if (recentFilm != '' && recentFilm != null && recentFilm.length <= 50 && isNaN(recentFilmMark) === false) {
+			personalMoviesDB.movies[recentFilm] = recentFilmMark;
+			counter++;
+			console.log('done');
+		} else {
+			console.log('error');
+			continue;
+		}
+	}
+while (counter != 2);
+*/
+
 
 // Вариант решения через цикл for
 
@@ -48,7 +66,7 @@ for (let i = 0; i < 2; i++) {
 		i--;
 		continue;
 	}
-}
+} 
 
 console.log(personalMoviesDB);
 
